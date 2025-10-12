6ac64134ee056744f13aa97987b13b2cd5f24946
Anastasia641 <anastasia.petrusca@isa.utm.md>
2025-10-12 18:33:03 +0300

    Add files via upload

--- layout.tsx
+++ layout.tsx
@@ -1,9 +1,36 @@
-export default function AuthLayout({
-    children
-}: { children: React.ReactNode }) {
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from "next/navigation"
+import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
+
+interface DashboardType {
+    children: React.ReactNode;
+    params: Promise<{ storeId: string }>
+}
+
+export default async function Dashboard({children, params}: DashboardType) {
+    const { userId } = await auth();
+    const { storeId } = await params;
+
+    if (!userId) {
+        redirect('/sign-in')
+    }
+
+    const store = await prismadb?.store.findFirst({
+        where: {
+            id: storeId,
+            userId
+        }
+    })
+
+    if (!store) {
+        redirect('/');
+    }
+
     return (
-        <div className="flex items-center justify-center w-full h-full">
-            { children }
-        </div>
+        <>
+            <Navbar/>
+            {children}
+        </>
     )
 }
